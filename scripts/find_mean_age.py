df19d7d5f783f1bdfd5f4a21e1ed2c6bb8f2f149
Varun R <varunkrishrao@gmail.com>
2022-03-21 17:25:14 -0400

    Added script to find mean age

--- scripts/find_mean_age.py
+++ scripts/find_mean_age.py
@@ -3,11 +3,9 @@ import sys
 import glob
 import string
 import pandas as pd
-#os.chdir("../src")
-extension = 'csv'
-titles = []
-total_mean_name = "total_mean_vax_levels.csv"
-for k in range(0,11):
+
+vax_levels = 10
+for k in range(0,vax_levels):
     string_val = str(k)
     title_name = "../results/exp_1/raw_runs/run_*_"+string_val+"0.csv"
     print(title_name)
@@ -17,15 +15,12 @@ for k in range(0,11):
         all_filenames[j] = "../results/exp_1/raw_runs/"+os.path.basename(all_filenames[j])
     print(all_filenames)
     combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+    combined_csv["total_ages"] = combined_csv["age"]*combined_csv["value"]
     #export to csv
     total_sim_string_name = "total_sim_vax_level_"+str(k)+"0.csv"
     print("finished one")
     combined_csv.to_csv( total_sim_string_name, index=False, encoding='utf-8-sig')
-
-for k in range(0,11):
-    string_val = str(k)
-    title_name = "total_sim_vax_level_mean_"+string_val+"0.csv"
-    titles.append(title_name)
-
-combined_csv = pd.concat([pd.read_csv(f) for f in titles])
-combined_csv.to_csv( total_mean_name, index=False, encoding='utf-8-sig')
+    mean_combined_age_data = combined_csv.groupby(["t","vartype","age"])
+    mean_name = "total_sim_vax_level_mean_"+str(k)+"0.csv"
+    mean_combined_age_data.to_csv(mean_name)
+    
--- scripts/graph_lineplots.py
+++ scripts/graph_lineplots.py
@@ -15,9 +15,15 @@ wide_sim_quantile_data = pd.read_csv("total_sim_vax_level_quantile_10.csv")
 #print("N : ",max(sim_data["N"]))
 valueX1 = "Xsi1"
 valueX2 = "Xsi2"
+valueD = "XD"
 print(sim_data['vartype'])
 infected_data = sim_data.query('vartype == @valueX1')
+death_data = sim_data.query('vartype == @valueD')
 #sns.lineplot(data=infected_data,x="t",y="value",hue="vartype")
 g = sns.FacetGrid(infected_data,col="vv",height=2.5,col_wrap=3)
+g2 = sns.FacetGrid(death_data,col="vv",height=2.5,col_wrap=3)
 g.map(sns.lineplot,"t","value")
+g.set(ylim=(0,20000))
+g2.map(sns.lineplot,"t","value")
+#g2.set(ylim=(0,20000))
 plt.show()
